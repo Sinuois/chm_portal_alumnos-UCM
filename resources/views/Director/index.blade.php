8d489c532d12f53177b500c8f7ab29a1e0fc18f0
leonardojeldes <52269107+leonardojeldes@users.noreply.github.com>
2019-07-11 00:11:32 -0400

    Update index.blade.php

--- resources/views/Director/index.blade.php
+++ resources/views/Director/index.blade.php
@@ -1,7 +1,7 @@
-{{-- Restriccion de acceso - Leer comentario en layout.redirect--}}
+<!--{{-- Restriccion de acceso - Leer comentario en layout.redirect--}}
 @include('layout.redirect')
 
-@extends('layout.master')
+@extends('layout.master') -->
 
 @section('title')
   <title>Perfil Director de Carrera</title>
@@ -14,98 +14,109 @@
 @section('body')
   @foreach ($directors as $director) {{-- Obtención de los datos del Director de Carrera --}}
     @if ($director->id == Auth::user()->id) 
-
       <div class="row">
-        <div class="col s12"> 
-            <h5 class="left-align"><i class="material-icons">person</i><b>&nbspDatos Personales</b></h5>    
-            <div class="card horizontal z-depth-1"> {{--Creacion de card superiro con datos de usuario--}}
-                <div class="col s3 indigo" style="position: relative; top: 0px">
-                    <h6 style = "color:white;"><b>Nombre:</b></h6>
-                    <h6 style = "color:white;"><b>RUT:</b></h6>
-                    <h6 style = "color:white;"><b>Email:</b></h6>
-                    <h6 style = "color:white;"><b>Teléfono:</b></h6>
-                    <h6 style = "color:white;"><b>Celular:</b></h6>
-                </div>
-                <div class="col s9" style="position: relative; top: 0px"> 
-                    <h6>&nbsp{{$director->nombres}}&nbsp{{$director->apellidos}}</h6> 
-                    <h6>&nbsp{{$director->rut}}</h6>
-                    <h6>&nbsp{{$director->email}}</h6>
-                    <h6>&nbsp{{$director->telefono}}</h6>
-                    <h6>&nbsp{{$director->celular}}</h6>
-                </div>
-                <div class="col s2">  
-                    <div>    
-                        @if (empty(Auth::user()->foto))  
-                          <img src="/images/default.png" class="center-align" style="width:80%; position: relative; left: 47px; top: 2px">
-                        @else
-                          <img src="{{$director->foto}}" class="center-align" style="width:80%; position: relative; left: 47px; top: 2px">
-                        @endif
-                    </div>
-                </div>
-            </div>
+        <div class="col s4">     
+            <div class="card-panel z-depth-5"> <!--Rectangulito donde estará el título y el botón desplegable -->
+              <?php $direccion_imagen = Auth::user()->foto ?>
+              <div align="center">    
+                @if (empty(Auth::user()->foto))  
+                  <img src="/images/default.png") style="width:40%">
+                @else
+                  <img src="{{$director->foto}}") style="width:40%">
+                @endif
+              </div>
+              <h4><i class="material-icons">person</i>&nbsp{{$director->nombres}}&nbsp{{$director->apellidos}}</h4> 
+              
+              <div class="divider"></div>
+              <div class="section">
+                <h5><b>RUT</b></h5>
+                <p><b><i>&nbsp&nbsp{{$director->rut}}</i></b></p>
+              </div>
             </div>
         </div>      
-
-
-        <div class="row">
-          <div class="col s6">     
-            <div class="card-panel z-depth-1"> <!--Rectangulito donde estará el título y el botón desplegable -->
-              <h5 class="left-align"><b>&nbspInformación nº1</b></h5> 
+        <div class="col s8">            
+            <div class="card-panel z-depth-5"> <!--Rectángulo donde estará el título y el botón desplegable -->
+              <h4>Bienvenido, Director de Carrera</h4>  
             </div>
-          </div>      
-
-          <div class="col s6">            
-              <div class="card-panel z-depth-1"> <!--Rectangulito donde estará el título y el botón desplegable -->
-                <h5 class="left-align"><b>&nbspInformación nº2</b></h5>
-                <ul class="collapsible"> <!--Collapsible de información-->
-                  <li>
-                    <div class="collapsible-header"><i class="material-icons">chrome_reader_mode</i>
-                      &nbsp<b>Información X</b></h6> </div>
-                    <div class="collapsible-body">
-                      <span>
-                        <div class="section">
-                          1
-                        </div> 
-                      </span>
-                    </div>
-                    <div class="collapsible-body">
-                      <span>
-                          <div class="section">
-                            2
-                          </div> 
-                        </span>
-                    </div>
-                    <div class="collapsible-body">
-                      <span>
-                        3
-                      </span>
-                    </div>
-                    <div class="collapsible-body">
-                      <span>
-                        <div class="section">
-                          4
-                        </div> 
-                      </span>
-                    </div>
-                  </li>
-                </ul>
-    
-                <ul class="collapsible"> <!--Collapsible de información-->
-                  <li>
-                    <div class="collapsible-header"><i class="material-icons">chrome_reader_mode</i>
-                      &nbsp<b>Información Y</b></h6> </div>
-                    <div class="collapsible-body">
-                      <span>
-                        <div class="section">
-                          <h7><b>Ejemplo título</b></h7>
-                          <p><i>&nbsp&nbspInformación respectiva al ejemplo.</i></p>
-                        </div> 
-                      </span>
-                    </div>
-                  </li>
-                </ul>
+            <ul class="collapsible"> <!--Collapsible de información-->
+            <li>
+              <div class="collapsible-header"><i class="material-icons">chrome_reader_mode</i>
+                &nbsp<b>Información Personal</b></h6> </div>
+              <div class="collapsible-body">
+                <span>
+                  <div class="section">
+                    <h7><b>Email</b></h7>
+                    <p><i>&nbsp&nbsp{{$director->email}}</i></p>
+                  </div> 
+                </span>
               </div>
-          </div>      
+              <div class="collapsible-body">
+                <span>
+                    <div class="section">
+                      <h7><b>Dirección</b></h7>
+                      <p><i>&nbsp&nbsp{{$director->direccion_actual}}</i></p>
+                    </div> 
+                  </span>
+              </div>
+              <div class="collapsible-body">
+                <span>
+                  <div class="section">
+                    <h7><b>Teléfono</b></h7>
+                    <p><i>&nbsp&nbsp{{$director->telefono}}</i></p>
+                  </div> 
+                </span>
+              </div>
+              <div class="collapsible-body">
+                <span>
+                  <div class="section">
+                    <h7><b>Celular</b></h7>
+                    <p><i>&nbsp&nbsp{{$director->celular}}</i></p>
+                  </div> 
+                </span>
+              </div>
+            </li>
+          </ul>
+
+          <ul class="collapsible"> <!--Collapsible de información-->
+            <li>
+              <div class="collapsible-header"><i class="material-icons">chrome_reader_mode</i>
+                &nbsp<b>Revision de tesis</b></h6> </div>
+              <div class="collapsible-body">
+                <span>
+                 <div class="section">
+                      <table class="table-border table-striped responsive-table">
+                              <thead>
+                                <tr>
+                                  <th>Codigo Inscripcion</th>
+                                  <th>Nombre de Tesis</th>
+                                  <th>Fecha Publicacion</th>
+                                  <th>revisar</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                <thead>
+                                       @foreach ($inscripciones as $inscripcion)
+                                         @if ($inscripcion->Estado=='Pendiente-D' )
+                                          <tr>
+                                            <td>{{$inscripcion->CodigoIncripcion}} </td>
+                                            <td>{{$inscripcion->Nombre_tesis}} </td>
+                                            <td>{{$inscripcion->FechaInscripcion}} </td>
+                                            <td>
+                                                  <a href="{{ url("/director/inscripciontesis/{$inscripcion->id}")}}" class="btn waves-effect waves-light" style="background-color: #253e85;">revisar</a>
+                                            </td>
+                                          </tr>
+                                          @endif
+                                        @endforeach
+                                     
+                                </thead>
+                              </tbody>
+                            </table>
+                    </div> 
+                </span>
+              </div>
+            </li>
+          </ul>
+        </div>      
       </div>
     @endif
   @endforeach
