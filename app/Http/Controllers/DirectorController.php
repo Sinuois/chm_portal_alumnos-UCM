440bd08f2d43a385f025d53cf8356ae5e07ce68d
leonardojeldes <52269107+leonardojeldes@users.noreply.github.com>
2019-07-10 23:41:38 -0400

    Update DirectorController.php

--- app/Http/Controllers/DirectorController.php
+++ app/Http/Controllers/DirectorController.php
@@ -4,42 +4,74 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\InscripcionesTesis;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class DirectorController extends Controller
 {
-    public function index()
+   public function index()
     {
-        $directors = User::All();
-        return view('Director.index', compact('directors'));
+
+
+        $id_director = Auth::user()->id;
+        $directors = User::where('id','=',$id_director)->get(); 
+
+
+         $inscripciones = DB::table('inscripciones_tesis')->where('Director_id', $id_director)->get();
+
+        return view('Director.index', compact('directors','inscripciones'));
     }
 
-     public function create()
+
+    public function inscripcionestesis($id)
     {
-        //
+
+       $inscripciones = new InscripcionesTesis;
+       $profesor = new User;
+       $alumnos = new User;
+       $directores = new User;
+
+       $id_d = Auth::user()->id;
+
+
+        $profesores = DB::table('users')->where('tipo_usuario', 'profesor')->get();
+        $alumnos = DB::table('users')->where('tipo_usuario', 'estudiante')->get();
+        $directores = DB::table('users')->where('id', $id_d)->get();
+      // dd($inscripcion);
+
+        $inscripciones = DB::table('inscripciones_tesis')->where('id', $id)->get();
+    //  dd($inscripciones);
+        return view ('Director.InscripcionTesis', compact('inscripciones','profesores','alumnos','directores'));
     }
 
-    public function store(Request $request)
+
+
+     public function guardarinscripcionestesis(Request $request)
     {
-        //
+        $inscripcion = new InscripcionesTesis;
+        $id_inscripcion = $request->input('id');
+
+
+
+      //  dd($request);
+
+        DB::table('inscripciones_tesis')
+            ->where('id', $id_inscripcion)
+            ->update(array('Estado' => 'Aprobado')
+        );
+
+       return redirect('/director');
     }
 
-    public function show($id)
+        public function listadotesis()
     {
-        //
-    }
+       $inscripciones = new InscripcionesTesis;
+        $id = Auth::user()->id;
 
-    public function edit($id)
-    {
-        //
-    }
+          $inscripciones = DB::table('inscripciones_tesis')->where('Director_id', $id)->paginate(6);
+      // dd($inscripcion);
+        return view ('Director.Detalles', compact('inscripciones'));
 
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    public function destroy($id)
-    {
-        //
     }
 }
